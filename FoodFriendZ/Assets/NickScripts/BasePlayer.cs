2f614e8f1e9b94f2ae5c4783bfbadb9b7c93452e
Buscems <46265895+Buscems@users.noreply.github.com>
2019-09-10 11:19:58 -0400

    I added some work that I did on the main characters to set it up so that Dan can start programming the characters.

---
+++ FoodFriendZ/Assets/NickScripts/BasePlayer.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class BasePlayer : ScriptableObject
+{
+    public float speed;
+    [HideInInspector]
+    public Vector3 currentPosition;
+    public Vector3 currentDirection;
+
+    [Tooltip("Can be 'Melee', 'Range', or 'Builder'")]
+    public string attackType;
+
+    [Header("Melee Characters")]
+    public GameObject weapon;
+    [Tooltip("This is the position the weapon will be at when it is not being used.")]
+    public Vector3 awayPos;
+    [Tooltip("This is going to be the size of the weapon, z is always 1.")]
+    public Vector3 attackSize;
+    [Tooltip("This will be how far the weapon is from the player when it is activated.")]
+    public float offset;
+    [Tooltip("This will be how fast the sword attack plays")]
+    public float attackSpeed;
+    //[Tooltip("This is the amount that the sword will spin around the player when attacking.")]
+    //public float attackRange;
+    [HideInInspector]
+    public bool isAttacking;
+
+    [Header("Ranged Characters")]
+    public GameObject bullet;
+
+    [Header("Building Characters")]
+    public GameObject drop;
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        //this will be taking care of whether or not the player might accidentally have the wrong weapon for anything
+        if(attackType == "Melee")
+        {
+            bullet = null;
+            drop = null;
+        }
+        if (attackType == "Ranged")
+        {
+            weapon = null;
+            drop = null;
+        }
+        if (attackType == "builder")
+        {
+            weapon = null;
+            bullet = null;
+        }
+
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+
+        
+
+    }
+
+}
---
+++ FoodFriendZ/Assets/NickScripts/MainPlayer.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+using Rewired.ControllerExtensions;
+
+public class MainPlayer : MonoBehaviour
+{
+
+    //the following is in order to use rewired
+    [Tooltip("Reference for using rewired")]
+    private Player myPlayer;
+    [Header("Rewired")]
+    [Tooltip("Number identifier for each player, must be above 0")]
+    public int playerNum;
+
+    public BasePlayer currentChar;
+
+    float speed;
+
+    Rigidbody2D rb;
+    Vector3 velocity;
+
+    private void Awake()
+    {
+
+        //Rewired Code
+        myPlayer = ReInput.players.GetPlayer(playerNum - 1);
+        ReInput.ControllerConnectedEvent += OnControllerConnected;
+        CheckController(myPlayer);
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        currentChar.Start();
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        currentChar.Update();
+        speed = currentChar.speed;
+        currentChar.currentPosition = this.transform.position;
+
+        velocity.x = myPlayer.GetAxisRaw("MoveHorizontal");
+        velocity.y = myPlayer.GetAxisRaw("MoveVertical");
+
+        if(velocity.x != 0)
+        {
+            currentChar.currentDirection.x = velocity.x;
+        }
+        if (velocity.y != 0)
+        {
+            currentChar.currentDirection.y = velocity.y;
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+
+        rb.MovePosition(transform.position + velocity * Time.deltaTime);
+
+    }
+
+    //these two methods are for ReWired, if any of you guys have any questions about it I can answer them, but you don't need to worry about this for working on the game - Buscemi
+    void OnControllerConnected(ControllerStatusChangedEventArgs arg)
+    {
+        CheckController(myPlayer);
+    }
+
+    void CheckController(Player player)
+    {
+        foreach (Joystick joyStick in player.controllers.Joysticks)
+        {
+            var ds4 = joyStick.GetExtension<DualShock4Extension>();
+            if (ds4 == null) continue;//skip this if not DualShock4
+            switch (playerNum)
+            {
+                case 4:
+                    ds4.SetLightColor(Color.yellow);
+                    break;
+                case 3:
+                    ds4.SetLightColor(Color.green);
+                    break;
+                case 2:
+                    ds4.SetLightColor(Color.blue);
+                    break;
+                case 1:
+                    ds4.SetLightColor(Color.red);
+                    break;
+                default:
+                    ds4.SetLightColor(Color.white);
+                    Debug.LogError("Player Num is 0, please change to a number > 0");
+                    break;
+            }
+
+
+        }
+    }
+
+}
