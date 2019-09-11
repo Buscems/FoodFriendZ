dd95c701ce79f6a0cee18c0a3b64b7fc448fa526
GeorgiaAngeli <46467829+GeorgiaAngeli@users.noreply.github.com>
2019-09-11 18:24:13 -0400

    Redid the Power-Up Script
    i just got rid of my old stuff and put new stuff cuz were doing the system differntly. I didnt make any prefab power ups yet

--- FoodFriendZ/Assets/Scripts/PowerUps.cs
+++ FoodFriendZ/Assets/Scripts/PowerUps.cs
@@ -4,104 +4,12 @@ using UnityEngine;
 
 public class PowerUps : MonoBehaviour
 {
-    //holds the type of powerUps that I can set
-    public enum PowerUpTypes
-    {
-        SlowSpeed,
-        FastSpeed,
-        SlowTime
+    [Tooltip("Power-Up Names")]
+    public string powerUpName = "";
 
-    }
-    //lets me set up the typer of powerUp
-    public PowerUpTypes currentPowerUp;
+    public float movementSpeed = 1;
+    public float attackSize = 1;
+    public float attackSpeed = 1;
+    public float attackDamage = 1;
 
-    //PowerUp Variables
-    public float multiplier;
-    public float timer = 5f;
-
-    void OnTriggerEnter2D(Collider2D other)
-    {
-
-            if (other.CompareTag("Player"))
-            {
-                StartCoroutine(Pickup(other));
-
-            }
-        
-    }
-
-   IEnumerator Pickup(Collider2D player)
-   {
-        PlayerStatTemp stats = player.GetComponent<PlayerStatTemp>();
-
-        if (stats.activeItem == false)
-        {
-            switch (currentPowerUp)
-            {
-                //case 1
-                case PowerUpTypes.FastSpeed:
-
-                    stats.activeItem = true;
-
-                    multiplier = 1.5f;
-                    stats.playerSpeed *= multiplier;
-
-                    GetComponent<SpriteRenderer>().enabled = false;
-                    GetComponent<Collider2D>().enabled = false;
-
-                    yield return new WaitForSeconds(timer);
-
-                    stats.playerSpeed /= multiplier;
-
-                    Destroy(gameObject);
-                    stats.activeItem = false;
-
-                    break;
-
-                //case 2
-                case PowerUpTypes.SlowSpeed:
-
-                    stats.activeItem = true;
-
-                    multiplier = .75f;
-                    stats.playerSpeed *= multiplier;
-
-                    GetComponent<SpriteRenderer>().enabled = false;
-                    GetComponent<Collider2D>().enabled = false;
-
-                    yield return new WaitForSeconds(timer);
-
-                    stats.playerSpeed /= multiplier;
-
-                    Destroy(gameObject);
-                    stats.activeItem = false;
-
-                    break;
-
-                //case 3
-                case PowerUpTypes.SlowTime:
-
-                    stats.activeItem = true;
-
-                    multiplier = .75f;
-                    Time.timeScale *= multiplier;
-
-                    GetComponent<SpriteRenderer>().enabled = false;
-                    GetComponent<Collider2D>().enabled = false;
-
-                    yield return new WaitForSeconds(timer);
-
-                    Time.timeScale /= multiplier;
-
-                    Destroy(gameObject);
-                    stats.activeItem = false;
-
-                    break;
-
-                default:
-                    Debug.Log("Not a Power Up");
-                    break;
-            }
-        }
-   }
-}//END OF SCRIPT
+}
