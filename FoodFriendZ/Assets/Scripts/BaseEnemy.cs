2f586f4392e802296f6ea1c48addc671fa637a20
Buscems <46265895+Buscems@users.noreply.github.com>
2019-09-12 11:19:06 -0400

    More scriptable objects
    I started setting up the scriptable object for the enemy types and started work on enemy aggro.

--- FoodFriendZ/Assets/Scripts/BaseEnemy.cs
+++ FoodFriendZ/Assets/Scripts/BaseEnemy.cs
@@ -2,17 +2,76 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseEnemy : MonoBehaviour
+[CreateAssetMenu]
+public class BaseEnemy : ScriptableObject
 {
+
+    [Header("Generic Enemy Values")]
+    [Tooltip("How fast we want the enemy to move")]
+    public float speed;
+    [Tooltip("How far away we want the enemy to be able to see the player")]
+    public float aggroRange;
+    [Tooltip("How long we want the enemy to be aggro after losing sight of the player")]
+    public float aggroTimerMax;
+
+    float aggroTimer;
+
+    Vector3 currentPos;
+
+    bool aggro;
+
+    Transform target;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+
+
+
+
     }
+
+    void Aggro()
+    {
+
+        if (aggro)
+        {
+
+            if (aggroTimer <= 0)
+            {
+                aggroTimer = aggroTimerMax;
+                aggro = false;
+            }
+
+        }
+
+        if ((target.transform.position - currentPos).magnitude < aggroRange)
+        {
+            aggro = true;
+            aggroTimer = aggroTimerMax;
+        }
+        else
+        {
+            if (aggro)
+            {
+                aggroTimer -= Time.deltaTime;
+            }
+        }
+
+    }
+
+    void Movement()
+    {
+
+    }
+
+    
+
 }
